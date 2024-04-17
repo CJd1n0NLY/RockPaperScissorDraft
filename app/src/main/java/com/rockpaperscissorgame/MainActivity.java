f6dd5428eb514c65f9b62d767607348546ebd7ae
seajey.p <postradocharles.bsit@gmail.com>
2024-04-17 01:07:53 -0700

    RockPaperScissor_Draft

--- app/src/main/java/com/rockpaperscissorgame/MainActivity.java
+++ app/src/main/java/com/rockpaperscissorgame/MainActivity.java
@@ -1,7 +1,9 @@
 package com.rockpaperscissorgame;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -16,24 +18,27 @@ import androidx.core.view.WindowInsetsCompat;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private static int userIndex;
+    private DatabaseHelper dbHelper;
    private static String selectedHandSign;
     private static final String[] handSigns = {"Rock", "Paper", "Scissors"};
     private ImageView[] pictures;
     private int wins = 0;
     private int losses = 0;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
 
         ImageView rockImage = findViewById(R.id.rockImage);
         ImageView paperImage = findViewById(R.id.paperImage);
         ImageView scissorsImage = findViewById(R.id.scissorsImage);
         Button playButton = findViewById(R.id.playButton);
-//        Button historyButton = findViewById(R.id.historyButton);
+        Button historyButton = findViewById(R.id.historyButton);
+
+        dbHelper = new DatabaseHelper(this);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -42,23 +47,27 @@ public class MainActivity extends AppCompatActivity {
         });
 
         pictures = new ImageView[]{
-                findViewById(R.id.rockImage),
-                findViewById(R.id.paperImage),
-                findViewById(R.id.scissorsImage)
+                rockImage,
+                paperImage,
+                scissorsImage
+
         };
 
         rockImage.setOnClickListener(v -> {
+             userIndex = 0;
             selectedHandSign = "Rock";
             displayUserSelected(selectedHandSign);
 
         });
 
         paperImage.setOnClickListener(v -> {
+             userIndex = 1;
             selectedHandSign = "Paper";
             displayUserSelected(selectedHandSign);
         });
 
         scissorsImage.setOnClickListener(v -> {
+             userIndex = 2;
             selectedHandSign = "Scissors";
             displayUserSelected(selectedHandSign);
         });
@@ -66,9 +75,18 @@ public class MainActivity extends AppCompatActivity {
         playButton.setOnClickListener(v -> {
             playGame();
         });
+
+        historyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
-    private void displayToast(String message, int duration) {
+    private void displayToast(String message) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
 
         Handler handler = new Handler();
@@ -77,16 +95,22 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
                 Toast.makeText(MainActivity.this, "", Toast.LENGTH_SHORT).cancel();
             }
-        }, 500);
+        }, 1000);
     }
 
-
     private void displayUserSelected(String handSign) {
-        displayToast("Selected Hand Sign: " + handSign, 1000);    }
+        displayToast("Selected Hand Sign: " + handSign);
+    }
 
     private void playGame() {
+
+        TextView whoWins = findViewById(R.id.lblWhoWins);
+        TextView userVsComputer = findViewById(R.id.userVsComputer);
+
+        String result;
+
         if (selectedHandSign == null) {
-            displayToast("Please select a hand sign first", 1000);
+            displayToast("Please select a hand sign first");
             return;
         }
 
@@ -94,20 +118,34 @@ public class MainActivity extends AppCompatActivity {
         int computerIndex = random.nextInt(handSigns.length);
         String computerHandSign = handSigns[computerIndex];
 
+        String userHandSign = handSigns[userIndex];
+
+        int userImage = getImage(userHandSign);
         int computerImage = getImage(computerHandSign);
 
         ImageView dispHands = findViewById(R.id.dispHands);
-        dispHands.setImageResource(computerImage);
+        ImageView userHands = findViewById(R.id.userHands);
+        ImageView computerHands = findViewById(R.id.computerHands);
+
+        dispHands.setVisibility(View.INVISIBLE);
+        userHands.setImageResource(userImage);
+        computerHands.setImageResource(computerImage);
 
         if (selectedHandSign.equals(computerHandSign)) {
-            displayToast("It's a tie!", 1000);
+            result = "It's a tie!";
+            displayToast(result);
+            whoWins.setText(result);
         } else if ((selectedHandSign.equals("Rock") && computerHandSign.equals("Scissors")) ||
                 (selectedHandSign.equals("Paper") && computerHandSign.equals("Rock")) ||
                 (selectedHandSign.equals("Scissors") && computerHandSign.equals("Paper"))) {
-            displayToast("User Wins!", 1000);
+            result = "Player Wins!";
+            displayToast(result);
+            whoWins.setText(result);
             wins++;
         } else {
-            displayToast("Computer Wins!", 1000);
+            result = "Computer Wins!";
+            displayToast(result);
+            whoWins.setText(result);
             losses++;
         }
 
@@ -117,7 +155,17 @@ public class MainActivity extends AppCompatActivity {
         TextView lblLose = findViewById(R.id.lblLose);
         lblLose.setText(String.valueOf(losses));
 
+        String userVbot = "Player : " + selectedHandSign + " VS " + "Bot : " + computerHandSign;
+        userVsComputer.setText(userVbot);
+
+        try{
+            dbHelper.addToHistory(selectedHandSign, computerHandSign, whoWins.getText().toString().trim(), "04-17-2024");
+        } catch (Exception e){
+            Toast.makeText(MainActivity.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+
         selectedHandSign = null;
+
 
     }
 
